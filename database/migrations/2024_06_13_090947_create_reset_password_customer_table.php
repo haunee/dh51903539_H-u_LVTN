6d8e52c87bcc4541d204448406ba5b3f408ac5da
Your Name <you@example.com>
2024-06-13 17:10:03 +0700

    *

--- database/migrations/2024_06_13_090947_create_reset_password_customer_table.php
+++ database/migrations/2024_06_13_090947_create_reset_password_customer_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('reset_password_customer', function (Blueprint $table) {
             $table->string('email')->unique();
             $table->string('token')->nullable();
+            
             $table->timestamps();
         });
     }
